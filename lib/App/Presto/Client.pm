b6e1c7103c79bfcfa1a05d375479c61fd448bc5f
Brian Phillips <bphillips@cpan.org>
2012-09-07 21:42:57 +0000

    Import of BPHILLIPS/App-Presto-0.001 from CPAN.
    gitpan-cpan-distribution: App-Presto
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         BPHILLIPS/App-Presto-0.001.tar.gz
    gitpan-cpan-author:       BPHILLIPS
    gitpan-cpan-maturity:     released

---
+++ lib/App/Presto/Client.pm
@@ -0,0 +1,163 @@
+package App::Presto::Client;
+BEGIN {
+  $App::Presto::Client::AUTHORITY = 'cpan:BPHILLIPS';
+}
+{
+  $App::Presto::Client::VERSION = '0.001';
+}
+
+# ABSTRACT: The REST client
+
+use strict;
+use warnings;
+use Moo;
+use REST::Client;
+use URI;
+use URI::QueryParam;
+use Module::Pluggable
+  instantiate => 'new',
+  sub_name    => 'content_handlers',
+  search_path => ['App::Presto::Client::ContentHandlers'];
+
+has config => ( is => 'ro', required => 1 );
+has _rest_client => ( is => 'lazy', );
+
+sub _build__rest_client {
+	my $self = shift;
+	return REST::Client->new;
+}
+
+sub all_headers {
+	my $self = shift;
+	my $headers = $self->_rest_client->{_headers} || {};
+	return %$headers;
+}
+
+sub set_header {
+	my $self = shift;
+	return $self->_rest_client->addHeader(@_);
+}
+
+sub get_header {
+	my $self    = shift;
+	my $header  = shift;
+	my $headers = $self->_rest_client->{_headers} || {};
+	return exists $headers->{$header} ? $headers->{$header} : undef;
+}
+
+sub clear_headers {
+	my $self = shift;
+	%{$self->_rest_client->{_headers} || {}} = ();
+	return;
+}
+
+sub GET {
+	my $self = shift;
+	my $uri  = $self->_make_uri(@_);
+	$self->_rest_client->GET($uri);
+}
+
+sub HEAD {
+	my $self = shift;
+	my $uri  = $self->_make_uri(@_);
+	$self->_rest_client->HEAD($uri);
+}
+
+sub DELETE {
+	my $self = shift;
+	my $uri  = $self->_make_uri(@_);
+	$self->_rest_client->DELETE($uri);
+}
+
+sub POST {
+	my $self = shift;
+	my $uri  = $self->_make_uri(shift);
+	$self->_rest_client->POST( $uri, shift );
+}
+
+sub PUT {
+	my $self = shift;
+	my $uri  = $self->_make_uri(shift);
+	$self->_rest_client->PUT( $uri, shift );
+}
+
+sub _make_uri {
+	my $self      = shift;
+	my $local_uri = shift;
+	my @args      = @_;
+	my $config    = $self->config;
+
+	my $endpoint;
+	$local_uri = '/' if ! defined $local_uri;
+	if ( $local_uri =~ m{^https?://} ) {
+		$endpoint = $local_uri;
+	} else {
+		$endpoint = $config->endpoint;
+		$endpoint .= '*' unless $endpoint =~ m/\*/;
+		$endpoint =~ s{\*}{$local_uri};
+	}
+
+	my $u = $self->_append_query_params( $endpoint, @args );
+	return "$u";
+}
+
+sub _append_query_params {
+	my $self = shift;
+	my @args = @_;
+	my $u    = URI->new(shift);
+	foreach my $next (@args) {
+		$u->query_param_append( split( /=/, $next, 2 ) );
+	}
+	return $u;
+}
+
+sub response {
+	return shift->_rest_client->{_res} || undef;
+}
+
+sub has_response_content {
+	my $self = shift;
+	return $self->response->content_length
+	  || length( $self->response->content );
+}
+
+sub response_data {
+	my $self     = shift;
+	my $response = $self->response;
+	if ( my $content_type = $response->header('Content-type') ) {
+		foreach my $h ( $self->content_handlers ) {
+			if ( $h->can_deserialize($content_type) ) {
+				return $h->deserialize( $response->content );
+			}
+		}
+		warn "no available deserializer found for content type: $content_type";
+	}
+	return $response->decoded_content;
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::Presto::Client - The REST client
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Brian Phillips <bphillips@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Brian Phillips and Shutterstock Images (http://shutterstock.com).
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/App/Presto/ShellUI.pm
@@ -0,0 +1,70 @@
+package App::Presto::ShellUI;
+BEGIN {
+  $App::Presto::ShellUI::AUTHORITY = 'cpan:BPHILLIPS';
+}
+{
+  $App::Presto::ShellUI::VERSION = '0.001';
+}
+
+# ABSTRACT: Term::ShellUI sub-class
+
+use strict;
+use warnings;
+use Regexp::Common qw(balanced);
+use Moo;
+use App::Presto::ArgProcessor;
+extends 'Term::ShellUI';
+
+foreach my $m(qw(readline GetHistory)){
+	no strict 'refs';
+	*$m = sub { my $self = $_[0]; my $target = $self->{term}->can($m); $_[0] = $self->{term}; goto $target };
+}
+
+has arg_processor => (
+	is       => 'lazy',
+);
+
+sub _build_arg_processor {
+	my $self = shift;
+	return App::Presto::ArgProcessor->new;
+}
+
+sub call_command {
+	my $self = shift;
+	my($cmd) = @_;
+	my $args = $cmd->{args};
+	eval {
+		$self->arg_processor->process($args);
+		1;
+	} or do {
+		warn "Error preparsing args @$args: $@";
+	};
+	return $self->SUPER::call_command(@_);
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::Presto::ShellUI - Term::ShellUI sub-class
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Brian Phillips <bphillips@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Brian Phillips and Shutterstock Images (http://shutterstock.com).
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
